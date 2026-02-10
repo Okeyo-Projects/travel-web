90893199f012530932a74e5a468912068ff296e2
NaimCode <naimdevelopper@gmail.com>
2026-02-10 01:03:55 +0100

    before chat persistency

--- src/providers/query-provider.tsx
+++ src/providers/query-provider.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 import { useState, type ReactNode } from 'react'
 
 export default function QueryProvider({ children }: { children: ReactNode }) {
@@ -19,7 +19,6 @@ export default function QueryProvider({ children }: { children: ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} client={queryClient as any}/>
     </QueryClientProvider>
   )
 }
